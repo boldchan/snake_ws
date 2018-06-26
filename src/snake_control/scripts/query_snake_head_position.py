190c7f53f9407fadaf68f8eb6ccb48f72cb9f759
peng chen <peng.chen@tum.de>
2018-06-26 12:04:16 +0200

    + query the relative position in snake_head coordinate

--- src/snake_control/scripts/query_snake_head_position.py
+++ src/snake_control/scripts/query_snake_head_position.py
@@ -3,6 +3,7 @@ import rospy
 from std_msgs.msg import Header
 from gazebo_msgs.srv import GetModelState, GetModelStateRequest
 from snake_control.msg import snake_head_rel_pos
+import numpy as np
 
 rospy.init_node('snake_head_pos_pub')
 
@@ -17,13 +18,32 @@ model = GetModelStateRequest()
 model.model_name = 'robot'
 model.relative_entity_name = 'target::link'
 
+model2 = GetModelStateRequest()#to get coordinate of snake head
+model2.model_name='robot'
+model2.relative_entity_name = ''
 r = rospy.Rate(2)
 
 while not rospy.is_shutdown():
     result = get_model_srv(model)
+    result2 = get_model_srv(model2)
 
-    pos.x_rel = result.pose.position.x
-    pos.y_rel = result.pose.position.y
+    x_rel_world = -result.pose.position.x
+    y_rel_world = -result.pose.position.y
+    z_rel_world = -result.pose.position.z
+
+    qx = result.pose.orientation.x
+    qy = result.pose.orientation.y
+    qz = result.pose.orientation.z
+    qw = result.pose.orientation.w
+
+    rotation_matrix = np.array([[1-2*qy*qy-2*qz*qz, 2*qx*qy-2*qz*qw, 2*qx*qz+2*qy*qw],
+	[2*qx*qy+2*qz*qw, 1-2*qx*qx-2*qz*qz,2*qy*qz-2*qx*qw],
+	[2*qx*qz-2*qy*qw, 2*qy*qz+2*qx*qw, 1-2*qx*qx-2*qy*qy]])  
+    
+    pos_rel = np.dot(rotation_matrix.T,np.array([x_rel_world, y_rel_world, z_rel_world]))
+    pos.x_rel = pos_rel[0]
+    pos.y_rel = pos_rel[1] 
+
 
     pos_pub.publish(pos)
 
